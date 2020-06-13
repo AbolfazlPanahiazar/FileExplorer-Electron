6d74a13346f4beed3344612fa2085f6d2408e1de
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-06-14 03:55:22 +0430

    Add intraction between rendererJs and mainJs with file names

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
+const fs = require("fs");
+const path = require("path");
 
 function createWindow() {
   // Create the browser window.
@@ -7,6 +9,7 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      enableRemoteModule: true,
     },
   });
 
@@ -25,9 +28,17 @@ app.on("window-all-closed", () => {
 });
 
 ////////////////////////////////
-//////////// IPCs //////////////
+//////////// IPCs response //////////////
 ////////////////////////////////
-ipcMain.on("asynchronous-message", (event, arg) => {
-  console.log(arg); // prints "ping"
-  event.reply("asynchronous-reply", "pong");
+ipcMain.on("get files", (event, data) => {
+  // If it's in the root and want to return back
+  if (data == "Close") {
+    app.quit();
+  }
+  // Read file and response the inner files
+  else {
+    fs.readdir(data, (err, files) => {
+      event.returnValue = JSON.stringify(files);
+    });
+  }
 });
--- renderer.js
+++ renderer.js
@@ -1,6 +1,10 @@
 const { ipcRenderer } = require("electron");
 
-ipcRenderer.on("asynchronous-reply", (event, arg) => {
-  console.log(arg); // prints "pong"
+// Add eventlistener on click
+const items = document.getElementsByClassName("item");
+Array.prototype.forEach.call(items, function (item) {
+  item.addEventListener("click", function () {
+    const newFiles = ipcRenderer.sendSync("get files", item.children[1].innerText);
+    console.log(newFiles);
+  });
 });
-ipcRenderer.send("asynchronous-message", "ping");
